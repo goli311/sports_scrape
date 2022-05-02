bea1c1ecc47dd4f1632088f1ffdee7bbd3968763
goli311 <99311698+goli311@users.noreply.github.com>
2022-05-02 18:39:58 +0530

    Update spy1_d.py

--- sports_subscribe_scrape/sports_subscribe_scrape/spiders/spy1_d.py
+++ sports_subscribe_scrape/sports_subscribe_scrape/spiders/spy1_d.py
@@ -16,7 +16,7 @@ def user_agent_get():
     user_agent_rotator = UserAgent(software_names=software_names, operating_systems=operating_systems, limit=100)
     user_agent = user_agent_rotator.get_random_user_agent()
     return user_agent
-
+#test git
 
 class MainScrape(scrapy.Spider):
     name = 'mycrawler1_d'

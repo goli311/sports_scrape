c7e86e7aba48e689eab5f87bddbf41972d0b8c06
Parth <golihathi31@gmail.com>
2022-05-03 10:36:09 +0530

    comment removed in config

--- sports_subscribe_scrape/sports_subscribe_scrape/config.py
+++ sports_subscribe_scrape/sports_subscribe_scrape/config.py
@@ -2,7 +2,6 @@ db_host = 'localhost'
 db_user = 'root'
 db_password = 'M$p@1234'
 database = 'sportsadmin_scrape'
-# database = 'sportsadmin_scrape'
 db_data_table = 'rbc_main'
 db_data_table_pitcher = 'rbc_main_pitchers'
 db_data_table_copy="rbc_main_child"
